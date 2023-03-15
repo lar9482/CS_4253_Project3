15599a844773f7bb7a5b53ae0fbbddb4c8bf4b29
Luke Runnels <lar9482@utulsa.edu>
2023-03-15 12:00:56 -0500

    Got the other player to move towards its goal once it obtains the ball

--- src/projects/proj2/evaluation.py
+++ src/projects/proj2/evaluation.py
@@ -15,26 +15,83 @@ def soccer(state, player_id):
     # SoccerState-specific information. The file
     # `src/lib/game/discrete_soccer.py` provides a description of all
     # useful SoccerState properties.
-    if (state.current_player_obj.has_ball):
-        print()
-    
-    
+
+    #Total utility
+    utility = 0
+
+    #Case where the current player has the ball
+    if (state.current_player_obj == state.player_with_ball):
+
+        #Get position of the current's player's goal
+        curr_goal = curr_player_goal(state.current_player_obj, state)
+
+        #Get position of current player
+        curr_pos = curr_player_pos(state)
+
+        #Get position of other player
+        other_pos = other_player_pos(state.current_player_obj, state)
+
+        #Calc distance between current player and its goal
+        dis_curr_goal = calculate_distance(
+            curr_pos[0],
+            curr_goal[0],
+            curr_pos[1],
+            curr_goal[1]
+        )
+
+        #Calc distance between current player and the other player
+        dis_curr_other = calculate_distance(
+            curr_pos[0],
+            other_pos[0],
+            curr_pos[1],
+            other_pos[1]
+        )
+
+        #Take the sum of distance between goal minus the other player
+        utility += (dis_curr_goal) - (dis_curr_other)
 
     #Case where neither player has the ball.
     if (state.objects[0].has_ball == False and state.objects[1].has_ball == False):
-        dis = calculate_distance(state.current_player_obj, state.objects[2])
-        return 0.75*(1/dis)
+        player_pos = curr_player_pos(state)
+        dis = calculate_distance(player_pos[0], 
+                                 state.objects[2].x,
+                                 player_pos[1],
+                                 state.objects[2].y)
+        utility += 0.5*(1/dis)
     
     if not isinstance(state, discrete_soccer.SoccerState):
         raise ValueError("Evaluation function incompatible with game type.")
     
-    return 10
+    return utility
 
-def calculate_distance(object1, object2):
-    x_term = (object1.x - object2.x) ** 2
-    y_term = (object1.y - object2.y) ** 2
+def calculate_distance(x1, x2, y1, y2):
+    x_term = (x1 - x2) ** 2
+    y_term = (y1 - y2) ** 2
 
     return math.sqrt(x_term + y_term)
+
+def curr_player_goal(player, state):
+    #Case where the current player is on the red team
+    if (player.team == 1):
+        #Return the blue goal position
+        return state.blue_goal_pos
+    #Case where the current player is on the blue team
+    else:
+        #Return the red goal position
+        return state.red_goal_pos
+    
+def other_player_pos(player, state):
+
+    #Case where the current is on the red team
+    if (player.team == 1):
+        #Return the coords of the playaer on the blue team
+        return (state.objects[1].x, state.objects[1].y)
+    else:
+        #Return the coords of the player on the red team
+        return (state.objects[0].x, state.objects[0].y)
+
+def curr_player_pos(state):
+    return (state.current_player_obj.x, state.current_player_obj.y)
 
 def connect_four(state, player_id):
     if not isinstance(state, connect_four.Connect4State):
