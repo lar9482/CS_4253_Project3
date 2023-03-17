a96d78a73071d25a9c172ac93c9e93381aa55146
Luke Runnels <lar9482@utulsa.edu>
2023-03-17 18:47:39 -0500

    Actually got the agent to move towards its goal and score a win.

--- src/projects/proj2/evaluation.py
+++ src/projects/proj2/evaluation.py
@@ -3,23 +3,12 @@
 import math, random
 from lib.game import discrete_soccer, connect_four
 
+
+#The main evaluation function that is used in the soccer game
 def soccer(state, player_id):
-    # TODO: Implement this function!
-    #
-    # The soccer evaluation function *must* look into the game state
-    # when running. It will then return a number, where the larger the
-    # number, the better the expected reward (or lower bound reward)
-    # will be.
-    #
-    # For a good evaluation function, you will need to
-    # SoccerState-specific information. The file
-    # `src/lib/game/discrete_soccer.py` provides a description of all
-    # useful SoccerState properties.
-
-
+    
     #Case where a win/loss is detected in the game
     if (state.is_terminal != None):
-        print(player_id)
         #If the winning team did not belong to the "current" player
         #Indicating the player who just played won the game
         if (state.is_terminal != state.objects[player_id].team):
@@ -35,15 +24,15 @@ def soccer(state, player_id):
             return 0
 
     #Case where the current player has the ball
-    elif (state.current_player_obj == state.player_with_ball):
+    elif (state.objects[player_id] == state.player_with_ball):
         #Get position of the current's player's goal
-        curr_goal = curr_player_goal(state.current_player_obj, state)
+        curr_goal = curr_player_goal(state.objects[player_id], state)
 
         #Get position of current player
-        curr_pos = curr_player_pos(state)
+        curr_pos = curr_player_pos(state, player_id)
 
         #Get position of other player
-        other_pos = other_player_pos(state.current_player_obj, state)
+        other_pos = other_player_pos(state.objects[player_id], state)
 
         #Calc distance between current player and its goal
         dis_curr_goal = calculate_distance(
@@ -60,34 +49,33 @@ def soccer(state, player_id):
             curr_pos[1],
             other_pos[1]
         )
-
+        #Calc distance between other player and the current player's goal
         dis_other_goal = calculate_distance(
             other_pos[0],
             curr_goal[0],
             other_pos[1],
             curr_goal[1]
         )
-
-        #If the other player is farther away from the current player's goal
-        if (dis_other_goal > dis_curr_goal):
+        #If the other player is not inbetween
+        if (dis_other_goal > dis_curr_other):
             #Return the minimum distance between current player and goal
-            return dis_curr_goal
+            return (15/dis_curr_goal)
         else:
             #Else, take the other player into account
-            return (dis_curr_goal) - (dis_curr_other)
+            return (15/dis_curr_goal) - dis_curr_goal
+
 
     #Case where neither player has the ball.
     elif (state.objects[0].has_ball == False and state.objects[1].has_ball == False):
-        player_pos = curr_player_pos(state)
+        player_pos = curr_player_pos(state, player_id)
 
         #Calculate distance between the current player's position and the ball's position
         dis = calculate_distance(player_pos[0], 
                                  state.objects[2].x,
                                  player_pos[1],
                                  state.objects[2].y)
-        
-        #Try to minimize the distance between current player and ball
-        return 0.5*(1/dis)
+        #Try to minimize the distance between current player and the ball
+        return  (1/dis)
     
     else:
         return 0
@@ -104,11 +92,11 @@ def curr_player_goal(player, state):
     #Case where the current player is on the red team
     if (player.team == 1):
         #Return the blue goal position
-        return state.blue_goal_pos
+        return state.red_goal_pos
     #Case where the current player is on the blue team
     else:
         #Return the red goal position
-        return state.red_goal_pos
+        return state.blue_goal_pos
     
 def other_player_pos(player, state):
 
@@ -122,8 +110,14 @@ def other_player_pos(player, state):
         #Return the coords of the player on the red team
         return (state.objects[0].x, state.objects[0].y)
 
-def curr_player_pos(state):
-    return (state.current_player_obj.x, state.current_player_obj.y)
+def curr_player_pos(state, player_id):
+    return (state.objects[player_id].x, state.objects[player_id].y)
+
+
+
+################################################################################################
+#Unused function below
+################################################################################################
 
 def connect_four(state, player_id):
     if not isinstance(state, connect_four.Connect4State):
